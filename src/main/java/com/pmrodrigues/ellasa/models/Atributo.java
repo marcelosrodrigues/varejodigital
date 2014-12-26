9f6b82ab90972cac86759b989e018585483d4d6b
Marcelo Rodrigues <marcelosrodrigues@globo.com>
2014-12-26 16:40:03 -0200

    corrigindo os testes unitário do projeto

--- src/main/java/com/pmrodrigues/ellasa/models/Atributo.java
+++ src/main/java/com/pmrodrigues/ellasa/models/Atributo.java
@@ -1,11 +1,7 @@
 package com.pmrodrigues.ellasa.models;
 
+import javax.persistence.*;
 import java.io.Serializable;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "atributo_produto")
@@ -14,6 +10,7 @@ public class Atributo implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column
